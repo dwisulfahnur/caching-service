42c590ad716da6e3d86c1011740ac1a32c0fedfd
Dwi Sulfahnur <dwisulfahnur@gmail.com>
2024-11-07 08:58:07 +0700

    feat: add schemas for payload request and response

---
+++ src/schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class PayloadRequest(BaseModel):
+    list_1: List[str]
+    list_2: List[str]
+
+
+class PayloadResponse(BaseModel):
+    id: str
+    output: str
