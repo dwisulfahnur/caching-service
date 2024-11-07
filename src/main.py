22fe88ea96a744ffeed6864d50e361c47fa14bd0
Dwi Sulfahnur <dwisulfahnur@gmail.com>
2024-11-07 11:07:23 +0700

    feat: add a new http middleware to calculate the process time of every request, then returning to the headers using headers key: x-process-time

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
+import time
 from fastapi import FastAPI
 from src.handlers import router
 
 app = FastAPI(title='Caching Service')
+
+
+@app.middleware("http")
+async def add_process_time_header(request, call_next):
+    """
+    this middleware will add a header to the response for every request.
+    will be useful to benchmark the request time.
+    """
+    start_time = time.monotonic()
+    response = await call_next(request)
+    process_time = time.monotonic() - start_time
+    response.headers["X-Process-Time"] = f"{process_time * 1000:.2f} ms"
+
+    return response
+
 
 app.include_router(router=router)
