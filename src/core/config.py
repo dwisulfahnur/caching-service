d7ea50cee08571fab5b7d9f52da1ed26374df473
Dwi Sulfahnur <dwisulfahnur@gmail.com>
2024-11-07 10:52:50 +0700

    feat: add autodetect if the app is running from pytest to determine the db_url to be use on the app

--- src/core/config.py
+++ src/core/config.py
@@ -1,6 +1,12 @@
 import os
+import sys
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./cache.db")
+TESTING = "pytest" in sys.modules or os.getenv("TESTING", "False") == "True"
+
+if TESTING:
+    DATABASE_URL = "sqlite:///./test_database.db"
+else:
+    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./cache.db")
--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -6,15 +6,18 @@ from src.main import app
 from src.core.database import get_dbsession
 from src.models import Payload
 
-
 # Create a separate database for test using SQLite engine
 TEST_DATABASE_URL = "sqlite:///./test_database.db"
-test_engine = create_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
+test_engine = create_engine(TEST_DATABASE_URL, connect_args={
+                            "check_same_thread": False})
 
 # Override the get_session dependency to use the test database
+
+
 def get_test_dbsession():
     with Session(test_engine) as session:
         yield session
+
 
 # Apply the override in the FastAPI app
 app.dependency_overrides[get_dbsession] = get_test_dbsession
@@ -24,16 +27,18 @@ app.dependency_overrides[get_dbsession] = get_test_dbsession
 def setup_database():
     # Create tables in the temporary SQLite database
     SQLModel.metadata.create_all(test_engine)
-    yield  # Run tests using this setup
+    yield
     # Teardown: Remove the test database file after the test session
     os.remove("test_database.db")
+
 
 @pytest.fixture(autouse=True)
 def clear_database():
     # Clear data from all tables before each test to ensure a fresh state
     with Session(test_engine) as session:
-        session.exec(delete(Payload))  # Truncate the Payload table or any other tables you have
+        session.exec(delete(Payload))  # Truncate the Payload table
         session.commit()
+
 
 @pytest.fixture
 def client():
